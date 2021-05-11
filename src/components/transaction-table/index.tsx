0143822b80181d1f8e6b11355fbb4a2953d74dcc
Luis Igreja <lfsigreja@gmail.com>
2021-05-11 15:24:01 -0300

    Aplication Structure

---
+++ src/components/transaction-table/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    useEffect(() => {
+        fetch('https://localhost:3000/api/transactions')
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }, []);
+
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td className='deposit'>R$2000</td>
+                        <td>Desenvolvimento </td>
+                        <td>11/05/21</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className='withdraw'> - R$1.100</td>
+                        <td>Desenvolvimento </td>
+                        <td>11/05/21</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
