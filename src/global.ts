783f1b70c21ce6b42fd16ee054217cb8ac636c18
Luis Igreja <lfsigreja@gmail.com>
2021-05-12 11:46:16 -0300

    Modal Styles

--- src/global.ts
+++ src/global.ts
@@ -57,4 +57,43 @@ export const GlobalStyle = createGlobalStyle`
         opacity: 0.6;
         cursor: not-allowed;
     }
+
+    .react-modal-overlay {
+        background: rgba(0,0,0,0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+
+        display:flex;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.25rem;
+    }
+
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+
+        transition: filter 0.2s;
+
+        &:hover {
+            /* filter: brightness(0); */
+            filter: invert(18%) sepia(100%) saturate(2202%) hue-rotate(334deg) brightness(113%) contrast(87%);
+        }
+    }
 `
