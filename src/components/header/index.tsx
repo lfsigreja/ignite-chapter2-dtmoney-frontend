acebbe284e3e2235440f7535f784a0ff455f2beb
Luis Igreja <lfsigreja@gmail.com>
2021-05-11 23:21:39 -0300

    cleaning code

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,5 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
-import { useState } from 'react'
-import  Modal from 'react-modal'
 
 interface HeaderProps {
     onOpenNewTransactionModal: () => void;
