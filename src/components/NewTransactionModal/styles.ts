04ba8bba11bc76190f0c348b00fb8d7489885249
Luis Igreja <lfsigreja@gmail.com>
2021-05-12 14:38:01 -0300

    Adding RadioBox class

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
 
 export const Container = styled.div`
     h2 {
@@ -53,9 +53,14 @@ export const TransactionTypeContainer = styled.div`
     grid-template-columns:1fr 1fr;
     gap: 0.5rem;
 `
-
 interface RadioBoxProps {
     isActive: boolean;
+    activeColor: "green" | "red";
+}
+
+const colors = {
+    green: '#33CC95',
+    red: '#E52E4D',
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -63,7 +68,8 @@ export const RadioBox = styled.button<RadioBoxProps>`
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: ${(props) => props.isActive ? '#d7d7d7' : 'transparent'};
+        background: ${
+        (props) => props.isActive ? transparentize(0.9, colors[props.activeColor]) : 'transparent'};
 
         display: flex;
         align-items: center;
