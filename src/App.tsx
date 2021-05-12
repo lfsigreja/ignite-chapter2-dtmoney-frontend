5fae621d30ef0d66034a13300bd39c6b24b8cebd
Luis Igreja <lfsigreja@gmail.com>
2021-05-11 23:16:19 -0300

    Modal Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
+import Modal from 'react-modal'
+import { useState } from 'react'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/header/index"
 
 import { GlobalStyle } from "./global"
 
+
+
 export function App() {
-  return (
+
+    const [ isNewTransactionModalOpen ,setIsNewTransactionModalOpen] = useState(false)
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false)
+    }
+
+    return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header  onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <Modal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h1>test</h1>
+      </Modal>
     </>
   );
 }
