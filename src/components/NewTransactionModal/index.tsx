f473dbaa05eb3d3c2cee9b597dc1ef762c7e27f4
Luis Igreja <lfsigreja@gmail.com>
2021-05-12 12:49:37 -0300

    deposit/withdraw-button-style

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,22 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react'
+
+
+
+
 interface NewTransactionModalProps { 
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps) {
+    
+    const [type, setType] = useState('deposit') 
+
     return (
         <Modal 
         isOpen={isOpen} 
@@ -20,10 +30,32 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
             >
                 <img src={closeImg} alt="Fechar Modal" />
             </button>
+
             <Container>
                 <h2>Cadastrar Transação</h2>
                 <input placeholder='Título' />
                 <input type="number" placeholder="Valor" />
+
+                <TransactionTypeContainer>
+                    <RadioBox 
+                    type='button' 
+                    onClick={() => {setType('deposit')}}
+                    isActive={type === 'deposit'}
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </RadioBox>
+
+                    <RadioBox 
+                    type='button' 
+                    onClick={() => {setType('withdraw')}}
+                    isActive={type === 'withdraw'}
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
+
                 <input placeholder='Categoria' />
                 <button type="submit">Cadastrar</button>
             </Container>
