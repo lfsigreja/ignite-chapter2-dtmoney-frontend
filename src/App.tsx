b7c82d2061ffbdab5fb676159bc02ba1c28c0c33
Luis Igreja <lfsigreja@gmail.com>
2021-05-12 11:10:07 -0300

    NewTransactionModal Componentization

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,28 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/header/index"
 
 import { GlobalStyle } from "./global"
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 Modal.setAppElement('#root')
 
 export function App() {
 
-    const [ isNewTransactionModalOpen ,setIsNewTransactionModalOpen] = useState(false)
+  const [ isNewTransactionModalOpen ,setIsNewTransactionModalOpen] = useState(false)
 
-    function handleOpenNewTransactionModal() {
-        setIsNewTransactionModalOpen(true)
-    }
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true)
+  }
 
-    function handleCloseNewTransactionModal() {
-        setIsNewTransactionModalOpen(false)
-    }
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false)
+  }
 
     return (
     <>
       <GlobalStyle />
       <Header  onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
-      <Modal 
-        isOpen={isNewTransactionModalOpen} 
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h1>test</h1>
-      </Modal>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
     </>
   );
 }
